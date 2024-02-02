2ce71167111415c18dd6f13d423d876a113c9dd5
BuBuCatt <jiayuhsu8240@gmail.com>
2024-02-01 17:49:16 -0800

    remove last version of  project section

--- main.js
+++
@@ -1,8 +0,0 @@
-// for the hamburger menu active 
-
-function toggleMenu() {
-  const menu = document.querySelector(".nav-menu");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
-}
